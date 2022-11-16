f45e15645283191bbcfc5016810fb6ef6199a886
Sridhar316 <88523603+Sridhar316@users.noreply.github.com>
2022-11-16 19:07:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,10 +8,7 @@ variable "cidr_block" {
     type = list(string)
     default = ["10.0.0.0/16", "10.0.1.0/24","10.0.2.0/24"]
 }
-variable "ami" {
-    type = string
-    default = "ami-09d3b3274b6c5d4aa"
-}
+
 
 variable "instance_type" {
     type = string
