1bc68bbe99cd4a9f086261a8eee60545360a9208
Sridhar316 <88523603+Sridhar316@users.noreply.github.com>
2022-11-17 12:59:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "aws" {
       region = "us-east-1"
-      access_key = "AKIAX7IEASGWIDWKLIXB"
-      secret_key = "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"
+      profile = "orgn"
       
 }
