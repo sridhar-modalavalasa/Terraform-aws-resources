01b1d3f78153645968af33684517a10df93a1848
Sridhar316 <88523603+Sridhar316@users.noreply.github.com>
2022-11-17 14:04:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  shared_credentials_files = ["/root/.aws/credentials"]
-  profile                  = "default"
+      region     = "${var.region}"
+      access_key = "${var.access_key}"
+      secret_key = "${var.secret_key}"
 }
